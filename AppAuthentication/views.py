63829269f09124dbb9a46f39810b639d6b1d84f0
Raag98 <raianurag9800@gmail.com>
2021-04-01 23:47:58 +0530

    commit1

--- AppAuthentication/views.py
+++ AppAuthentication/views.py
@@ -9,7 +9,7 @@ from Trello.models import Task, TaskList
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
             return redirect('login')
     return render(request, 'AppAuthentication/register.html')
